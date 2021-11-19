799c7330287b3b96a1fc4b3efa4b52c9df788b55
eugenepauljean <89397312+eugenepauljean@users.noreply.github.com>
2021-11-19 15:13:34 +0100

    Update network-setup.sh

--- network-setup.sh
+++ network-setup.sh
@@ -11,17 +11,17 @@
 #                     GATEWAY       (Enter the Gateway default route)        #
 ##############################################################################
 ### ETHERNET CARDS ASSIGNATIONS ##############################################
-eth1-mac=""
-eth1-name="wan"
-eth1-ip=""
-eth1-mask="32"
-eth1-gw=""
+eth1mac=""
+eth1name="wan"
+eth1ip=""
+eth1mask="32"
+eth1gw=""
 
-eth2-mac=""
-eth2-name="lan"
-eth2-ip=""
-eth2-mask="24"
-eth2-gw=""
+eth2mac=""
+eth2name="lan"
+eth2ip=""
+eth2mask="24"
+eth2gw=""
 ##############################################################################
 ### REMOVE Autoname ethernet configuration
 rmautoname=/etc/udev/rules.d/80-net-name-slot.rules
@@ -30,10 +30,9 @@ ln -s /dev/null $rmautoname
 
 ### CREATE udev rules and define interface name manually
 echo "\
-SUBSYSTEM=="net", ATTRS{address}=="$eth1-mac", NAME="$eth1-name" \
-SUBSYSTEM=="net", ATTRS{address}=="$eth2-mac", NAME="$eth2-name" \
+SUBSYSTEM=="net", ATTRS{address}=="$eth1mac", NAME="$eth1name" \
+SUBSYSTEM=="net", ATTRS{address}=="$eth2mac", NAME="$eth2name" \
 " > /etc/udev/rules.d/10-network.rules
-
 
 ### CREATE SYSTEMD Network Service
 echo "\
@@ -41,27 +40,25 @@ echo "\
 Description=Network Connectivity
 Wants=network.target
 Before=network.target
-BindsTo=sys-subsystem-net-devices-network.device
-After=sys-subsystem-net-devices-network.device
 
 [Service]
 Type=oneshot
 RemainAfterExit=yes
 
-# Fixed IP address for $eth1-name + $eth2-name
-ExecStart=/sbin/ip link set dev $eth1-name up
-ExecStart=/sbin/ip link set dev $eth2-name up
-ExecStart=/sbin/ip addr add $eth1-name-ip/$eth1-name-mask dev $eth1-name
-ExecStart=/sbin/ip addr add $eth2-name-ip/$eth2-name-mask dev $eth2-name
+# Fixed IP address for $eth1name + $eth2name
+ExecStart=/sbin/ip link set dev $eth1name up
+ExecStart=/sbin/ip link set dev $eth2name up
+ExecStart=/sbin/ip addr add $eth1ip/$eth1mask dev $eth1name
+ExecStart=/sbin/ip addr add $eth2ip/$eth2mask dev $eth2name
 
-# Define DEFAULT Gateway for $eth1-name
-ExecStart=/sbin/ip route add default via $eth1-name-gw dev $eth1-name onlink
+# Define DEFAULT Gateway for $eth1name
+ExecStart=/sbin/ip route add default via $eth1gw dev $eth1name onlink
 
 # Flush and Interface down AFTER SystemD service STOP
-ExecStop=/sbin/ip addr flush dev $eth1-name
-ExecStop=/sbin/ip addr flush dev $eth2-name
-ExecStop=/sbin/ip link set dev $eth1-name down
-ExecStop=/sbin/ip link set dev $eth2-name down
+ExecStop=/sbin/ip addr flush dev $eth1name
+ExecStop=/sbin/ip addr flush dev $eth2name
+ExecStop=/sbin/ip link set dev $eth1name down
+ExecStop=/sbin/ip link set dev $eth2name down
 
 [Install]
 WantedBy=multi-user.target
